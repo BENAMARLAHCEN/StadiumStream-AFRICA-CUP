a885e81540f3ea2751d464a1e9b9b11c584c2fa2
mohamed <simotergui4@gmail.com>
2023-12-27 12:33:09 +0100

    integration page about

---
+++ app/View/about .php
@@ -0,0 +1,65 @@
+<section class="container  pt-5  ">
+    <div class="row justify-content-center">
+        <div class="col-md">
+            <h1 class="font-weight-bold">
+                About Us
+            </h1>
+            <div>
+                <h2 style="color: #004930;">
+                    <span style=" color :#FF9B26;">Welcome to StadiumStream</span> - Your Gateway to Live Experiences!
+                </h2>
+                <p>
+                    At StadiumStream, we believe that every live event is a story waiting to be written, and we're here
+                    to help
+                    you script your own unforgettable moments. As your go-to platform for ticket reservations, we've
+                    made it our
+                    mission to simplify the process of securing your seat at the hottest games, concerts, and events.
+                    Get ready
+                    to elevate your live experience with StadiumStream.
+                </p>
+                <button class="p-2 pr-3 pl-3  text-light "
+                    style="background-color: #FF7900; border-radius: 1rem 1rem ; border: #FF7900;">
+                    Join Us
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M6.41634 13.0833C6.26357 12.9306 6.18357 12.7361 6.17634 12.5C6.16912 12.2639 6.24218 12.0694 6.39551 11.9167L10.4788 7.83333H1.16634C0.930233 7.83333 0.732178 7.75333 0.572178 7.59333C0.412178 7.43333 0.332455 7.23556 0.333011 7C0.333011 6.76389 0.413011 6.56583 0.573011 6.40583C0.733011 6.24583 0.930788 6.16611 1.16634 6.16667H10.4788L6.39551 2.08333C6.24273 1.93056 6.16968 1.73611 6.17634 1.5C6.18301 1.26389 6.26301 1.06944 6.41634 0.916666C6.56912 0.763889 6.76357 0.6875 6.99968 0.6875C7.23579 0.6875 7.43023 0.763889 7.58301 0.916666L13.083 6.41667C13.1663 6.48611 13.2255 6.57306 13.2605 6.6775C13.2955 6.78194 13.3127 6.88944 13.3122 7C13.3122 7.11111 13.295 7.21528 13.2605 7.3125C13.2261 7.40972 13.1669 7.5 13.083 7.58333L7.58301 13.0833C7.43023 13.2361 7.23579 13.3125 6.99968 13.3125C6.76357 13.3125 6.56912 13.2361 6.41634 13.0833Z"
+                            fill="white" />
+                    </svg>
+
+                </button>
+            </div>
+        </div>
+        <div class="col-md row  d-md-inline d-none ">
+            <img style="width: 35vw; height: 35vw;" src="./Capture d’écran 2023-12-26 120400.png" alt="">
+
+        </div>
+    </div>
+</section>
+
+<section class="container mx-auto row   pb-5 pt-5">
+    <div style="padding-right:5rem;" class="col-md d-md-inline d-none  ">
+        <img style="width: 35vw; height: 39vw;" src="./Capture d’écran 2023-12-26 121347.png" alt="">
+    </div>
+    <div class=" col-md  pt-5 ">
+        <h2 style="color: #004930;">Who We Are</h2>
+        <p>
+            StadiumStream isn't just a ticketing website; we're your trusted partner in making your event dreams a
+            reality.
+            Whether you're a sports fanatic, a music lover, or a theater buff, we've curated a diverse selection of
+            tickets
+            to cater to every passion and interest. With StadiumStream, you're not just attending an event; you're
+            creating
+            memories that last a lifetime.
+        </p>
+        <button class="p-2 pr-3 pl-3 text-light"
+            style="background-color: #FF7900; border-radius: 1rem 1rem ; border: #FF7900;">
+            Learn More
+            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M6.41634 13.0833C6.26357 12.9306 6.18357 12.7361 6.17634 12.5C6.16912 12.2639 6.24218 12.0694 6.39551 11.9167L10.4788 7.83333H1.16634C0.930233 7.83333 0.732178 7.75333 0.572178 7.59333C0.412178 7.43333 0.332455 7.23556 0.333011 7C0.333011 6.76389 0.413011 6.56583 0.573011 6.40583C0.733011 6.24583 0.930788 6.16611 1.16634 6.16667H10.4788L6.39551 2.08333C6.24273 1.93056 6.16968 1.73611 6.17634 1.5C6.18301 1.26389 6.26301 1.06944 6.41634 0.916666C6.56912 0.763889 6.76357 0.6875 6.99968 0.6875C7.23579 0.6875 7.43023 0.763889 7.58301 0.916666L13.083 6.41667C13.1663 6.48611 13.2255 6.57306 13.2605 6.6775C13.2955 6.78194 13.3127 6.88944 13.3122 7C13.3122 7.11111 13.295 7.21528 13.2605 7.3125C13.2261 7.40972 13.1669 7.5 13.083 7.58333L7.58301 13.0833C7.43023 13.2361 7.23579 13.3125 6.99968 13.3125C6.76357 13.3125 6.56912 13.2361 6.41634 13.0833Z"
+                    fill="white" />
+            </svg>
+
+        </button>
+    </div>
+</section>
