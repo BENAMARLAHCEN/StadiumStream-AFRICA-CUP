f1251c5fb52ccb104139d276ebf676b2d3eb0f03
nadaelmahfoudi <elmahfoudinada17@gmail.com>
2024-01-02 11:15:53 +0100

    adding Stadium's modal

---
+++ app/Model/Stadium.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Model;
+
+class Team extends Crud{
+
+    private int $id;
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function selectAllStadium(){
+        return $this->selectRecords('stadiums');
+    }
+    public function selectStadium($id){
+        return $this->selectRecords('stadiums','*','id = '.$id);
+    }
+    public function addStadium(array $data){
+        return $this->insertRecord('stadiums',$data);
+    }
+    public function DeleteStadium(int $id){
+        return $this->deleteRecord('stadiums',$id);
+    }
+    public function UpdateStadium(array $data,int $id){
+        return $this->updateRecord('stadiums',$data,$id);
+    }
+}
