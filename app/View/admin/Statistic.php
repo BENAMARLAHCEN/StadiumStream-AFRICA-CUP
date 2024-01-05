3de9f54d3724422cb953324cdae5b2907626b1fa
lahcen <lahcen.ben3@gmail.com>
2024-01-05 11:17:14 +0100

    statistic view

---
+++ app/View/admin/Statistic.php
@@ -0,0 +1,33 @@
+<section class="section dashboard">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Static</h5>
+            <div class="row">
+             <div class="col-4">
+            <div class="card">
+                <div class="row">
+                    <div class="col"><h1>Team</h1></div>
+                    <div class="col"><?=$team['teamCount']?></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <div class="row">
+                    <div class="col"><h1>stadium</h1></div>
+                    <div class="col"><?=$team['StadiumCount']?></div>
+                </div>
+            </div>
+            </div>
+            <div class="col-4">
+            <div class="card">
+                <div class="row">
+                    <div class="col-6"><h1>match</h1></div>
+                    <div class="col-6"><?=$team['MatchCount']?></div>
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
